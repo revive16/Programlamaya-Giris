3e4e3f1d42690095dcc8ff52911931dc6752eabe
revive16 <59067593+revive16@users.noreply.github.com>
2019-12-21 00:21:59 +0300

    Java Dilinde TersKosegen

---
+++ TersKosegen.java
@@ -0,0 +1,27 @@
+ package justforpractise;
+
+import java.util.Scanner;
+
+public class JustForPractise {
+
+  
+    public static void main(String[] args) {
+ 
+ 
+ 
+ System.out.print("Enter A Number Please: ");   
+            int enter=input.nextInt();     //Kosegen Uzunlugununu Kullanicidan Aliyoruz
+            
+            for(int i=enter; i>=1 ;i--){ 
+                    
+                    
+                    for(int j=1; j<=enter ;j++){
+                        if(i==j){System.out.print(1);}
+                        else{System.out.print(0);}
+                    }
+                    System.out.println("");
+                    
+            }
+            
+            }
+            }
